f105f4181928522f9097afcaaf14f7037f199f78
Tylor Shin <shincode@icloud.com>
2018-09-12 13:57:01 +0900

    Add scinapse-feedback package

--- handler.ts
+++ handler.ts
@@ -5,16 +5,10 @@ import { JWT } from "google-auth-library";
 import { rejects } from "assert";
 import { resolve } from "path";
 
-interface Feedback {
-  content: string;
-  email?: string;
-  userId?: string;
-  gaId?: string;
-  createdAt?: string;
-}
-
 // POST https://qg6wp4ze48.execute-api.us-east-1.amazonaws.com/prod/feedbacks/new
 export async function handleFeedback(event, context, callback) {
+  console.log(event);
+
   const SLACK_SCINAPSE_FEEDBACK_WEBHOOK_URL =
     process.env.SLACK_SCINAPSE_FEEDBACK_WEBHOOK_URL;
   const GOOGLE_SHEET_CLIENT_EMAIL = process.env.GOOGLE_SHEET_CLIENT_EMAIL;
@@ -22,17 +16,17 @@ export async function handleFeedback(event, context, callback) {
   const SPREAD_SHEET_ID = "14jL4Lw56018fbcBPMsQCe2wSULW05EMDEmheLElnI90";
   let token;
 
-  // if (!SLACK_SCINAPSE_FEEDBACK_WEBHOOK_URL) {
-  //   throw new Error("SLACK TOKEN is missing");
-  // }
+  if (!SLACK_SCINAPSE_FEEDBACK_WEBHOOK_URL) {
+    throw new Error("SLACK TOKEN is missing");
+  }
 
-  // try {
-  //   await axios.post(SLACK_SCINAPSE_FEEDBACK_WEBHOOK_URL, {
-  //     text: "TEST"
-  //   });
-  // } catch (err) {
-  //   console.error(err);
-  // }
+  try {
+    await axios.post(SLACK_SCINAPSE_FEEDBACK_WEBHOOK_URL, {
+      text: "TEST"
+    });
+  } catch (err) {
+    console.error(err);
+  }
 
   const SCOPES = ["https://www.googleapis.com/auth/spreadsheets"];
 
@@ -79,9 +73,7 @@ export async function handleFeedback(event, context, callback) {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      SLACK_TOKEN: process.env.SLACK_TOKEN,
-      message: "Go Serverless v1.0! Your function executed successfully!",
-      env: process.env
+      event
     })
   };
 
